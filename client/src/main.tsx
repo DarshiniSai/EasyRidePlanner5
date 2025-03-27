719695b901808cf4c5fd75f38b27d612695ea228
Darshini Sai Mathi <159278720+DarshiniSai@users.noreply.github.com>
2025-03-27 09:23:17 +0530

    Delete client directory

--- client/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-// Create root element and render the app
-createRoot(document.getElementById("root")!).render(
-  <App />
-);
