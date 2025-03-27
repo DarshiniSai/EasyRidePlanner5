ad186ec0425d5cc240bc72be40583b84a457c4a4
Darshini Sai Mathi <159278720+DarshiniSai@users.noreply.github.com>
2025-03-27 09:23:05 +0530

    Delete attached_assets directory

--- attached_assets/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-// Simplified version without Clerk authentication for demo
-createRoot(document.getElementById("root")!).render(
-  <App />
-);
